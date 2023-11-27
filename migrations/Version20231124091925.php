841b01af31782fe5468f141130afb0b6172b60e0
jekuaitk <jekua@aarhus.dk>
2023-11-27 12:59:21 +0100

    Added comments to views and used nowdoc

--- migrations/Version20231124091925.php
+++ migrations/Version20231124091925.php
@@ -19,17 +19,21 @@ final class Version20231124091925 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql(
-            'CREATE OR REPLACE VIEW organisation AS
-                SELECT
-                    organisation_enhed_registrering.organisation_enhed_id AS id,
-                    organisation_enhed_registrering_egenskab.enhed_navn AS enhedsnavn,
-                    organisation_enhed_registrering_overordnet.reference_id_uuididentifikator AS overordnet_id
-                FROM organisation_enhed_registrering
-                LEFT OUTER JOIN organisation_enhed_registrering_egenskab ON organisation_enhed_registrering.id = organisation_enhed_registrering_egenskab.organisation_enhed_registrering_id
-                LEFT OUTER JOIN organisation_enhed_registrering_overordnet ON organisation_enhed_registrering.overordnet_id = organisation_enhed_registrering_overordnet.id
-            ;'
-        );
+        $this->addSql(<<<'SQL'
+CREATE OR REPLACE VIEW organisation AS
+    SELECT
+        organisation_enhed_registrering.organisation_enhed_id AS id,
+        organisation_enhed_registrering_egenskab.enhed_navn AS enhedsnavn,
+        organisation_enhed_registrering_overordnet.reference_id_uuididentifikator AS overordnet_id
+    FROM organisation_enhed_registrering
+
+    -- Add enhedsnavn
+    LEFT OUTER JOIN organisation_enhed_registrering_egenskab ON organisation_enhed_registrering.id = organisation_enhed_registrering_egenskab.organisation_enhed_registrering_id
+
+    -- Add overordnet_id
+    LEFT OUTER JOIN organisation_enhed_registrering_overordnet ON organisation_enhed_registrering.overordnet_id = organisation_enhed_registrering_overordnet.id
+;
+SQL);
     }
 
     public function down(Schema $schema): void
